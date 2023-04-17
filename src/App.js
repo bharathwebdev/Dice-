95d3077a074bd117d706ae0f049558974c6976e2
bharath <bharath18117886@gmail.com>
2023-04-17 23:20:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,83 @@
-import {useState } from 'react';
-import './App.css';
-import React from 'react'
-
-function App() {
-const [num,Setnum] = useState(0);
-// const [num2,Setnum2] = useState(0);
-const [p1,setp1] = useState([]);
-// const dice = [1,2,3,4,5,6];
-  const handleclick = ()=>{
-   const a =  Math.floor(Math.random()*(6-1+1))+1;
-   setp1(pre=>([...pre,a]));
-   Setnum(a);
-  }
-  return (
-    <div>
-         <div className='Root_1'>Dice
-          <div className='players'>
-            <div className='Player1'>Player-1</div>
-            <div style={{display:'flex',gap:'10px'}}>
+import { useState } from "react";
+import "./App.css";
+import React from "react";
 
 
-            {
-              p1.map(data=>{
-                return <p>{data}</p>
-              })
-            }
-            </div>
-            <p>{num}</p>
-            <div className='Player2'>Player-2</div>
-            </div>
-          {/* <button></button> */}
-          <div className='images'>
-          {num}
-         
-          <br></br>
-          <button className='Roll_btn' onClick={handleclick}>Roll</button>
-           </div>
-        </div>
-      
-      <div className='Root_1'></div>
-    </div>
-  )
+const  diceobj = {
+  1 :"../public/assets/dice-six-faces-five.png"
 }
 
-export default App                       
+function App() {
+  const [num, Setnum] = useState(0);
+  const [p1, setp1] = useState([]);
+  const [p2, setp2] = useState([]);
 
+  const [active, setActive] = useState(true);
+
+  const handleclick = () => {
+    const a = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+
+    if (a == 1 || a == 6) {
+      Setnum(0);
+      setActive((pre) => !pre);
+    }
+    // console.log(num);
+    if (active) {
+      setp1((pre) => [...pre, a]);
+    } else {
+      setp2((pre) => [...pre, a]);
+    }
+
+    // if (a === 1 || a === 6) {
+   
+    // }else{
+    //   console.log(a);
+      Setnum(a);
+    // }
+  };
+
+  return (
+    <div>
+      <div className="Root_1">
+        Dice
+        <div
+          className="players"
+          style={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+          }}
+        >
+          <div>
+            <div className="Player1">Player One{active && "🟢"}</div>
+            <div style={{ display: "flex", gap: "5px" ,}}>
+              {p1.map((data) => {
+                return <p> {data} </p>;
+              })}
+            </div>
+            <p>Player One got : {active && num}</p>
+          </div>
+
+          <div>
+            <div className="Player2">Player Two{!active && "🟢"}</div>
+            <div style={{ display: "flex", gap: "10px" }}>
+              {p2.map((data) => {
+                return <p> {data} </p>;
+              })}
+            </div>
+            <p> Player Two got : {!active && num}</p>
+          </div>
+        </div>
+      </div>
+      <img style={{
+        width:'100px'
+      }} src={`assets/dice-six-faces-${num}.png`}/>
+      
+      <button className="Roll_btn" onClick={handleclick}>
+        ROLL
+      </button>
+    </div>
+  );
+}
+
+export default App;
